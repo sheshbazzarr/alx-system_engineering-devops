a5d3493363186adfc46a4119531df72dbaac0eba
sheshbazzarr <demerwdani@gmail.com>
2024-07-30 10:47:34 -0500

    commit -m

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 
+This script retrieves and displays the TODO list progress for a given employee using a REST API.
+
+Requirements:
+- Use the `requests` module to fetch data from the API.
+- Accept an integer as a parameter representing the employee ID.
+- Display the progress in the following format:
+  - First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+    - EMPLOYEE_NAME: name of the employee
+    - NUMBER_OF_DONE_TASKS: number of completed tasks
+    - TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
+  - Subsequent lines: Display titles of completed tasks with 1 tabulation and 1 space before the TASK_TITLE.
+
+"""
+import requests
+import sys
+if __name__=="__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = user = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    username = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    completed = [t.get("title") for t in user if t.get("completed") is True]
+    
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,5 +1,27 @@
 ## 0x15. API
 
+# 0-gather_data_from_an_API
+## TODO List Progress Script
+
+This Python script retrieves and displays the TODO list progress for a given employee using a REST API.
+
+## Requirements
+
+- Use the `requests` module to fetch data from the API.
+- Accept an integer as a parameter representing the employee ID.
+- Display the progress in the following format:
+  - **First line:** Employee `EMPLOYEE_NAME` is done with tasks(`NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS`):
+    - `EMPLOYEE_NAME`: name of the employee
+    - `NUMBER_OF_DONE_TASKS`: number of completed tasks
+    - `TOTAL_NUMBER_OF_TASKS`: total number of tasks, which is the sum of completed and non-completed tasks
+  - **Subsequent lines:** Display titles of completed tasks with 1 tabulation and 1 space before the `TASK_TITLE`.
+
+## Example Usage
+
+Run the script from the command line with the employee ID as an argument:
+
+```sh
+python script.py 1
 
 
 Date july 30,2024
