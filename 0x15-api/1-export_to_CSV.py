10b2b5e1e6e91d5fc7392cc7cf6e0489732eb1c0
sheshbazzarr <demerwdani@gmail.com>
2024-07-30 09:29:27 -0700

    naming error resovled

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,12 +6,12 @@ import sys
 if __name__=="__main__":
     Id=sys.argv[1]
     url="https://jsonplaceholder.tyicode.com/"
-    user = requests.get(url + "todos", params={"userId": id}).json()
-    users = requests.get(url + "users/{}".format(id)).json()
-    with open("{}.csv".format(id), "w", newline="") as filecsv:
+    user = requests.get(url + "todos", params={"userId": Id}).json()
+    users = requests.get(url + "users/{}".format(Id)).json()
+    with open("{}.csv".format(Id), "w", newline="") as filecsv:
         csv = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
         [csv.writerow(
-            [id, users.get("username"), t.get("completed"), t.get("title")]
+            [Id, users.get("username"), t.get("completed"), t.get("title")]
          ) for t in user]
 
     
