ec49fc1d9f556bb1191741b0dbbecc4c5bb55c33
sheshbazzarr <demerwdani@gmail.com>
2024-07-30 11:33:40 -0500

    change the code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" return csv"""
+"""Returns return todos for a given employee ID."""
 import csv
 import requests
 import sys
-if __name__=="__main__":
-    Id=sys.argv[1]
-    url="https://jsonplaceholder.tyicode.com/"
-    user = requests.get(url + "todos", params={"userId": Id}).json()
-    users = requests.get(url + "users/{}".format(Id)).json()
-    with open("{}.csv".format(Id), "w", newline="") as filecsv:
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "todos", params={"userId": id}).json()
+    users = requests.get(url + "users/{}".format(id)).json()
+    with open("{}.csv".format(id), "w", newline="") as filecsv:
         csv = csv.writer(filecsv, quoting=csv.QUOTE_ALL)
         [csv.writerow(
-            [Id, users.get("username"), t.get("completed"), t.get("title")]
+            [id, users.get("username"), t.get("completed"), t.get("title")]
          ) for t in user]
-
-    
