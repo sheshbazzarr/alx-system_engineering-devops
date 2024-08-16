144bfb566b84b3b26baad2d9e925e6a4568a721d
Sheshbazzar <99654542+sheshbazzarr@users.noreply.github.com>
2024-08-16 05:03:54 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Function to query subscribers on a given Reddit subreddit.."""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    "return total numm of sub"
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by u/sheshbazzaL)"
-    }   
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/sheshbazzaL)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
